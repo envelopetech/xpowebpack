e50401c11d02530b94fe786f9e3de60ee94f206f
Hetal Mehta <hetu.upadhyay@gmail.com>
2020-05-28 16:17:37 +0530

    trying to fix socket error

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
 export default function () {    
-    const socket = socketIOClient("http://localhost:5000", {'transports': ['websocket', 'polling']});
+    const socket = socketIOClient("https://xporiumwebpack.herokuapp.com", {'transports': ['websocket', 'polling']});
     function test() {         
         socket.emit('test')
     }    
