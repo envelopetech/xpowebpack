9bde1e6b8145c55c5b45c17ad2266943b338cf1e
Hetal Mehta <hetu.upadhyay@gmail.com>
2020-05-29 00:41:25 +0530

    remove env file

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -113,6 +113,9 @@ class Auth extends Component {
         this.setState({ islogin: false, signuptext: "Sign Up", signupsubtext: "Already have an account?" })
     }
     render() {
+
+        console.log(process.env.NODE_ENV.REACT_APP_GTM_ID); 
+        console.log(process.env.NODE_ENV.REACT_APP_OWN_URL);
         
         const { classes } = this.props;
         const responseFacebook = (response) => {
